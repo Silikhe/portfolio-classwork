8506fc0f6406219af7252d4976685926168f57a3
Sil silas <silaswans@gmail.com>
2020-09-23 05:26:21 +0300

    deploy portforlio class work

---
+++ frontend/views/site/about.php
@@ -0,0 +1,25 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-index">
+  <section  class="d-flex align-items-center">
+    <div class="container align-items-center" data-aos="zoom-in" data-aos-delay="50">
+      <h2><strong>About Me</strong></h2></br>
+      <h3>-Intovert</h3>
+      <h3>-A mentor Andela Learning Community.</h3>
+      <h3>-Aged 20</h3>
+      <h3>-Student Jkuat</h3>
+      <h3>-Certified google Developer<strong>Mobile Web</strong></h3>
+      <h3>- ❤️ code </h3></br>
+      
+      <h5><a  href="https://silsilas.github.io/portf/">More About me</a></h5>
+
+    </div>
+  </section>
+</div>
---
+++ frontend/views/site/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Portfolio';
+?>
+<div class="site-index">
+  <section id="hero" class="d-flex align-items-center">
+    <div class="container d-flex flex-column align-items-center" data-aos="zoom-in" data-aos-delay="100">
+      <h1>Silas Silikhe</h1>
+      <h2>I'm a Android and web Developer based in Nairobi.</h2>
+      <a href="about.html" class="btn-about">About Me</a>
+    </div>
+  </section>
+</div>
